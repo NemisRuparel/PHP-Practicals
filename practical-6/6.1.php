43979825c2cb117fa7551867db518dfca2b9ed0b
Nemis Ruparel <nemisruparel07@gmail.com>
2025-01-31 22:37:56 +0530

    committed

---
+++ practical-6/6.1.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+         * {
+            margin: 0;   
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body {
+            margin-top: 125px;
+            background-color: aquamarine;
+            font-family: Arial, sans-serif;
+        }
+        form input, form button {
+            display: block;
+            width: 50%;
+            margin: 10px auto;
+            padding: 10px;
+            border: 1px solid black;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.200), 0 6px 20px 0 rgba(0, 0, 0, 0.200);
+        }
+        .container{}
+        form input {
+            background-color: aliceblue;
+            height: 50px;
+            width: 625px;
+        }
+        form button {
+            width: 300px;
+            height: 50px;
+            transition: all .3s;
+            cursor: pointer;
+            font-size: 20px;
+            font-weight: bold;
+
+        }
+        form button:hover {
+            background-color: black;
+            color: white;
+        }
+        h1 {
+           text-align: center;
+            margin-bottom: 20px;
+            font-size: 60px;
+        }
+        #result {
+            text-align: center;
+            margin-top: 20px;
+            font-size: 24px;
+            font-weight: bold;
+            color: black;
+        }
+        .button-container {
+            display: flex;
+            justify-content: center;
+        }
+        p{
+            font-size: 30px;
+            text-align: center;
+        }
+        #btn1{
+            margin-left: 555px;
+        }
+        #btn2{
+            margin-left: -535px;
+        }
+        table{
+            border: 4px solid black;
+            margin-left: 380px;
+            border-collapse: collapse;
+            background-color:rgba(35, 200, 145, 0.53);
+        }
+        tr,td{
+            width: 500px;
+            border: 3px solid black;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <form action="6.1.php" method="POST">
+        <h1>String Functions</h1>
+        <input type="text"name="str" placeholder="Enter String">
+        <input type="text"name="str_word" placeholder="Enter String to replace">
+        <input type="text"name="str_replaced" placeholder="Enter String to replace with">
+        <div style="display:flex;">
+            <button type="submit" name="submit"id="btn1">Display</button>
+            <button name="clr"id="btn2">Clear Result</button>
+            
+        </div>
+        <br><br>
+        
+    </form>
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") 
+        {
+            $ipstr = $_POST["str"];
+            $lower_string=strtolower($ipstr);
+            $reversed_string=strrev($ipstr);
+            $trimmed_string=trim($ipstr);
+            $str_rep_word =$_POST["str_word"];
+            $str_rep_word_with = $_POST["str_replaced"];
+
+            if (isset($_POST["submit"]) && !empty($_POST["str"])) {
+                
+                
+                echo "
+                <h1>Result</h1>
+                <table>
+
+                <tr>
+                    <td><p><b>Main String</b></p></td>
+                    <td><p>$ipstr</p></td>
+                </tr>
+                <tr>
+                 <td><p><b>Lowered String</b></p></td>
+                 <td><p>$lower_string</p></td>
+                 </tr>
+                 <tr>
+                    <td><p><b>Reversed String</b></p></td>
+                    <td><p>$reversed_string</p></td>
+                </tr>
+                <tr>
+                <td><p><b>WhiteSpace Removed String</b></p>
+                <td><p>$trimmed_string</p></td>
+                </tr>
+                <tr>
+                <td><p><b>".$str_rep_word." Replaced With ".$str_rep_word_with." is</b></p></td>
+                <td><p>".str_replace($str_rep_word,$str_rep_word_with,$ipstr)."</p></td>
+                </td>
+                </table>
+                ";
+          
+                
+            }
+        }
+    ?>
+</body>
+</html>
---
+++ practical-6/6.2.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+         * {
+            margin: 0;   
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body {
+            margin-top: 125px;
+            background-color: aquamarine;
+            font-family: Arial, sans-serif;
+        }
+        form input, form button {
+            display: block;
+            width: 50%;
+            margin: 10px auto;
+            padding: 10px;
+            border: 1px solid black;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.200), 0 6px 20px 0 rgba(0, 0, 0, 0.200);
+        }
+        .btn{
+            display: flex;
+        }
+        #btn1{
+            margin-left: 560px;
+        }
+        #btn2{
+            margin-left: -535px;
+        }
+        form input {
+            background-color: aliceblue;
+            height: 50px;
+            width: 625px;
+        }
+        form button {
+         
+            width: 300px;
+            height: 50px;
+            transition: all .3s;
+            cursor: pointer;
+            font-size: 20px;
+            font-weight: bold;
+
+        }
+        form button:hover {
+            background-color: black;
+            color: white;
+        }
+        h1 {
+           text-align: center;
+            margin-bottom: 20px;
+            font-size: 60px;
+        }
+        #result {
+            text-align: center;
+            margin-top: 20px;
+            font-size: 24px;
+            font-weight: bold;
+            color: black;
+        }
+        .button-container {
+            display: flex;
+            justify-content: center;
+        }
+        p{
+            padding-top: 10px;
+            font-size: 30px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <b>Smallest Note Calculator</b>
+    </h1>
+    <form action="6.2.php"method="POST">
+        <input type="number"name="ipamount"placeholder="Ente Amount">
+        <div class="btn">
+            <button id="btn1"type="submit"name="calculate">Calculate</button>
+            <button id="btn2"name="clr">Clear</button>
+        </div>
+    </form>
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") 
+        {
+            if (isset($_POST["calculate"]) && empty($_POST["ipamount"])){
+                echo"<p>Please enter amount</p>";
+            }
+            if(isset($_POST["calculate"]) && !empty($_POST["ipamount"])){
+                $input = $_POST["ipamount"];
+                $denomination = [100,50,20,10,5,1];
+                $noteAmount = 0;
+    
+                if ($input <= 0 or $input == 0) {
+                    echo"<p>Please enter positive amount</p>";
+                    return;
+                }
+                echo "<h1 style='margin-top:70px'>Result</h1>";
+                echo"<p><b>Minimum notes for amount : </b>".$input."</p>";
+                foreach ($denomination as $value){
+                    $count = intdiv($input, $value);
+        $noteCount[$value] = $count;
+        $input %= $value;
+                }
+                foreach ($noteCount as $note => $count) {
+                    if ($count == 0) {
+                        continue;
+                    }
+                    echo "<p><b>Rs.".$note." : </b>". $count ." notes<br></p>";
+                }
+                
+                $totalNotes = array_sum($noteCount);
+                echo "<p><b>Minimum notes required: </b>"."$totalNotes"."</p>";
+            }
+        } 
+    ?>
+</body>
+</html>
+
+
