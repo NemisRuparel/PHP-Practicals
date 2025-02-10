7218034fe60e495ac54c4cc6ab297f0d1761e484
Nemis Ruparel <nemisruparel07@gmail.com>
2025-02-10 19:26:13 +0530

    commited

--- practical-7/7.1.php
+++ practical-7/7.1.php
@@ -4,12 +4,79 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        *{
+            color: black;
+        }
+        body{
+            background-color: aquamarine;
+        }
+        h2{
+            text-align: center;
+        }
+        hr{
+            width: 50%;
+            /* height: 2px; */
+            border: 1px solid black;
+            background-color: black;
+
+        }
+        .half1{
+            width: 48%;
+            border: 1px solid white;
+            margin-left: 51%;
+            border-radius: 10px;
+            margin-top: -300px;
+            background-color: white;
+        }
+        .container{
+            width: 48%;
+            border: 1px solid white;
+            color: black;
+            background-color: white;
+            border-radius: 10px;
+            padding-bottom: 10px;
+            padding-left: 10px;
+            margin-left: 1%;
+            
+        }
+        
+    </style>
 </head>
 <body>
-    <?php
-        
+    <div class="container">
+<h1>1. Class Definition (Base)</h1>
+<h3>Constructor (__construct())</h3>
+<p>Automatically called when an object of the class is created. <br>
+Prints "This is Constructor of class Base". <br></p>
+<h3>Public Method (Basemethod())</h3>
+A regular method that prints "Public method of class Base" when called. <br>
+<h3>Destructor (__destruct()) </h3>
 
+Automatically called when the script execution ends or the object is destroyed. <br>
+Prints "This is destructor of class Base". <br><br>
+<h1>2. Object Creation and Method Invocation</h1>
+An object $p1 of class Base is created, triggering the constructor.<br>
+The Basemethod() is called using $p1->Basemethod();  , displaying its message. <br>
+<strong>Once the script execution finishes, the destructor is automatically executed.</strong>
+</div>
+    <?php
+         class Base{
+           
+                function __construct(){
+                    echo "<div class='half1'><h2>This is Constructor of class Base<br><hr></h2>";
+                }
+                public function Basemethod(){
+                    echo "<h2>Public method of class Base <br><hr></h2>";
+                }
+                function __destruct(){
+                    echo "<h2>This is destructor of class Base</h2></div>";
+                }
+            }
+            
         
+        $p1 = new Base();
+        $p1->Basemethod();
     ?>
 </body>
 </html>
---
+++ practical-7/7.6.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        <style>* {
+            color: black;
+        }
+
+        body {
+            background-color: aquamarine;
+        }
+
+        h2 {
+            text-align: center;
+        }
+
+        hr {
+            width: 50%;
+            /* height: 2px; */
+            border: 1px solid black;
+            background-color: black;
+
+        }
+
+        .half1 {
+            width: 48%;
+            border: 1px solid white;
+            margin-left: 51%;
+            border-radius: 10px;
+            margin-top: -300px;
+            background-color: white;
+        }
+
+        .container {
+            width: 48%;
+            border: 1px solid white;
+            color: black;
+            background-color: white;
+            border-radius: 10px;
+            padding-bottom: 10px;
+            padding-left: 10px;
+            margin-left: 1%;
+
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>1. GrandParent Class (GrandParentClass)</h1>
+        <h3>Method: displayMessage()</h3>
+        <p>Prints "Parent Class:". <br>
+            This method will be overridden in the child class.
+            <br>
+        </p>
+        <h1>2. Child Class (ChildClass)</h1>
+        <h3>Inheritance</h3>
+        ChildClass extends ParentClass, meaning it inherits its methods.<br>
+        <h3>Overridden Method: displayMessage()</h3>
+        Calls the parent class method using parent::displayMessage();. <br>
+        Prints "Overridden Method Called Using Child Class:".<br><br>
+        <h1>3. Object Creation and Method Execution</h1>
+        A ChildClass object $childInstance is created. <br>
+        Calling $childInstance->displayMessage(); does the following: <br>
+        <b>
+            1. Executes parent::displayMessage();, which prints "Parent Class:". <br>
+            2. Then prints "Overridden Method Called Using Child Class:". <br>
+        </b>
+    </div>
+    <?php
+    class GrandParentClass
+    {
+        public function displayMessage()
+        {
+            echo "<div class='half1'>";
+            echo "<h2>Grand Parent Class:</h2><hr>";
+        }
+    }
+
+    class ParentClass extends GrandParentClass
+    {
+        public function displayMessage()
+        {
+            parent::displayMessage();
+            echo "<h2>Parent Class Method Calling GrandParent Class</h2>";
+            echo "</div>";
+
+        }
+
+        public function displayMessage1()
+        {
+            echo "<div class='half1'>";
+            echo "<h2>Parent Class:</h2><hr>";
+        }
+
+    }
+    class ChildClass extends ParentClass
+    {
+        public function displayMessage()
+        {
+            parent::displayMessage1();
+            echo "<h2>Child Class Method Calling Parent Class</h2>";
+            echo "</div>";
+
+        }
+    }
+    $childInstance = new ChildClass();
+    $childInstance->displayMessage();
+
+    $parentInstance = new ParentClass();
+    $parentInstance->displayMessage();
+
+
+    
+    ?>
+</body>
+
+</html>
